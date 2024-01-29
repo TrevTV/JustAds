cec47f64c5a974d3fd4f1d90f55995318ab11d32
Trevor <me@trev.app>
2024-01-28 19:03:42 -0500

    Cleanup player

--- src/player.js
+++ src/player.js
@@ -1,347 +1,174 @@
-const aiAds = [
+const ads = [
   /* allfoods_01 */
   [
-    { type: "video/webm", src: "./ads/allfoods_01/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/allfoods_01/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/allfoods_01/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/allfoods_01/-AD_TYPE-/h264.mp4" },
   ],
   /* allfoods_02 */
   [
-    { type: "video/webm", src: "./ads/allfoods_02/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/allfoods_02/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/allfoods_02/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/allfoods_02/-AD_TYPE-/h264.mp4" },
   ],
   /* all_world_insurance */
   [
-    { type: "video/webm", src: "./ads/all_world_insurance/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/all_world_insurance/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/all_world_insurance/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/all_world_insurance/-AD_TYPE-/h264.mp4" },
   ],
   /* biotechnica */
   [
-    { type: "video/webm", src: "./ads/biotechnica/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/biotechnica/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/biotechnica/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/biotechnica/-AD_TYPE-/h264.mp4" },
   ],
   /* body_implants */
   [
-    { type: "video/webm", src: "./ads/body_implants/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/body_implants/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/body_implants/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/body_implants/-AD_TYPE-/h264.mp4" },
   ],
   /* budget_arms_family */
   [
-    { type: "video/webm", src: "./ads/budget_arms_family/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/budget_arms_family/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/budget_arms_family/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/budget_arms_family/-AD_TYPE-/h264.mp4" },
   ],
   /* burrito_xxl */
   [
-    { type: "video/webm", src: "./ads/burrito_xxl/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/burrito_xxl/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/burrito_xxl/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/burrito_xxl/-AD_TYPE-/h264.mp4" },
   ],
   /* caliente */
   [
-    { type: "video/webm", src: "./ads/caliente/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/caliente/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/caliente/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/caliente/-AD_TYPE-/h264.mp4" },
   ],
   /* chromanticore */
   [
-    { type: "video/webm", src: "./ads/chromanticore/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/chromanticore/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/chromanticore/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/chromanticore/-AD_TYPE-/h264.mp4" },
   ],
   /* dtr */
   [
-    { type: "video/webm", src: "./ads/dtr/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/dtr/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/dtr/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/dtr/-AD_TYPE-/h264.mp4" },
   ],
   /* eji */
   [
-    { type: "video/webm", src: "./ads/eji/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/eji/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/eji/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/eji/-AD_TYPE-/h264.mp4" },
   ],
   /* foreign_body */
   [
-    { type: "video/webm", src: "./ads/foreign_body/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/foreign_body/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/foreign_body/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/foreign_body/-AD_TYPE-/h264.mp4" },
   ],
   /* kt_henry */
   [
-    { type: "video/webm", src: "./ads/kt_henry/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/kt_henry/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/kt_henry/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/kt_henry/-AD_TYPE-/h264.mp4" },
   ],
   /* milfgaard */
   [
-    { type: "video/webm", src: "./ads/milfgaard/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/milfgaard/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/milfgaard/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/milfgaard/-AD_TYPE-/h264.mp4" },
   ],
   /* moonchies */
   [
-    { type: "video/webm", src: "./ads/moonchies/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/moonchies/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/moonchies/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/moonchies/-AD_TYPE-/h264.mp4" },
   ],
   /* mrstud */
   [
-    { type: "video/webm", src: "./ads/mrstud/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/mrstud/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/mrstud/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/mrstud/-AD_TYPE-/h264.mp4" },
   ],
   /* mrwhitey */
   [
-    { type: "video/webm", src: "./ads/mrwhitey/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/mrwhitey/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/mrwhitey/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/mrwhitey/-AD_TYPE-/h264.mp4" },
   ],
   /* naranjita */
   [
-    { type: "video/webm", src: "./ads/naranjita/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/naranjita/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/naranjita/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/naranjita/-AD_TYPE-/h264.mp4" },
   ],
   /* nicola */
   [
-    { type: "video/webm", src: "./ads/nicola/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/nicola/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/nicola/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/nicola/-AD_TYPE-/h264.mp4" },
   ],
   /* orgiatic */
   [
-    { type: "video/webm", src: "./ads/orgiatic/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/orgiatic/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/orgiatic/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/orgiatic/-AD_TYPE-/h264.mp4" },
   ],
   /* peralez */
   [
-    { type: "video/webm", src: "./ads/peralez/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/peralez/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/peralez/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/peralez/-AD_TYPE-/h264.mp4" },
   ],
   /* rcs */
   [
-    { type: "video/webm", src: "./ads/rcs/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/rcs/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/rcs/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/rcs/-AD_TYPE-/h264.mp4" },
   ],
   /* real_water */
   [
-    { type: "video/webm", src: "./ads/real_water/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/real_water/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/real_water/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/real_water/-AD_TYPE-/h264.mp4" },
   ],
   /* slaughterhouse */
   [
-    { type: "video/webm", src: "./ads/slaughterhouse/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/slaughterhouse/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/slaughterhouse/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/slaughterhouse/-AD_TYPE-/h264.mp4" },
   ],
   /* sojasil */
   [
-    { type: "video/webm", src: "./ads/sojasil/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/sojasil/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/sojasil/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/sojasil/-AD_TYPE-/h264.mp4" },
   ],
   /* speed_loans */
   [
-    { type: "video/webm", src: "./ads/speed_loans/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/speed_loans/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/speed_loans/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/speed_loans/-AD_TYPE-/h264.mp4" },
   ],
   /* techtronika */
   [
-    { type: "video/webm", src: "./ads/techtronika/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/techtronika/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/techtronika/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/techtronika/-AD_TYPE-/h264.mp4" },
   ],
   /* thrud */
   [
-    { type: "video/webm", src: "./ads/thrud/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/thrud/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/thrud/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/thrud/-AD_TYPE-/h264.mp4" },
   ],
   /* tiancha */
   [
-    { type: "video/webm", src: "./ads/tiancha/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/tiancha/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/tiancha/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/tiancha/-AD_TYPE-/h264.mp4" },
   ],
   /* uscracks */
   [
-    { type: "video/webm", src: "./ads/uscracks/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/uscracks/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/uscracks/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/uscracks/-AD_TYPE-/h264.mp4" },
   ],
   /* vargas */
   [
-    { type: "video/webm", src: "./ads/vargas/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/vargas/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/vargas/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/vargas/-AD_TYPE-/h264.mp4" },
   ],
   /* watson_whore */
   [
-    { type: "video/webm", src: "./ads/watson_whore/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/watson_whore/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/watson_whore/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/watson_whore/-AD_TYPE-/h264.mp4" },
   ],
   /* wet_dream */
   [
-    { type: "video/webm", src: "./ads/wet_dream/ai/vp9.webm" },
-    { type: "video/mp4", src: "./ads/wet_dream/ai/h264.mp4" },
+    { type: "video/webm", src: "./ads/wet_dream/-AD_TYPE-/vp9.webm" },
+    { type: "video/mp4", src: "./ads/wet_dream/-AD_TYPE-/h264.mp4" },
   ],
 ];
 
-const rawAds = [
-  /* allfoods_01 */
-  [
-    { type: "video/webm", src: "./ads/allfoods_01/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/allfoods_01/raw/h264.mp4" },
-  ],
-  /* allfoods_02 */
-  [
-    { type: "video/webm", src: "./ads/allfoods_02/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/allfoods_02/raw/h264.mp4" },
-  ],
-  /* all_world_insurance */
-  [
-    { type: "video/webm", src: "./ads/all_world_insurance/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/all_world_insurance/raw/h264.mp4" },
-  ],
-  /* biotechnica */
-  [
-    { type: "video/webm", src: "./ads/biotechnica/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/biotechnica/raw/h264.mp4" },
-  ],
-  /* body_implants */
-  [
-    { type: "video/webm", src: "./ads/body_implants/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/body_implants/raw/h264.mp4" },
-  ],
-  /* budget_arms_family */
-  [
-    { type: "video/webm", src: "./ads/budget_arms_family/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/budget_arms_family/raw/h264.mp4" },
-  ],
-  /* burrito_xxl */
-  [
-    { type: "video/webm", src: "./ads/burrito_xxl/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/burrito_xxl/raw/h264.mp4" },
-  ],
-  /* caliente */
-  [
-    { type: "video/webm", src: "./ads/caliente/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/caliente/raw/h264.mp4" },
-  ],
-  /* chromanticore */
-  [
-    { type: "video/webm", src: "./ads/chromanticore/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/chromanticore/raw/h264.mp4" },
-  ],
-  /* dtr */
-  [
-    { type: "video/webm", src: "./ads/dtr/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/dtr/raw/h264.mp4" },
-  ],
-  /* eji */
-  [
-    { type: "video/webm", src: "./ads/eji/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/eji/raw/h264.mp4" },
-  ],
-  /* foreign_body */
-  [
-    { type: "video/webm", src: "./ads/foreign_body/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/foreign_body/raw/h264.mp4" },
-  ],
-  /* kt_henry */
-  [
-    { type: "video/webm", src: "./ads/kt_henry/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/kt_henry/raw/h264.mp4" },
-  ],
-  /* milfgaard */
-  [
-    { type: "video/webm", src: "./ads/milfgaard/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/milfgaard/raw/h264.mp4" },
-  ],
-  /* moonchies */
-  [
-    { type: "video/webm", src: "./ads/moonchies/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/moonchies/raw/h264.mp4" },
-  ],
-  /* mrstud */
-  [
-    { type: "video/webm", src: "./ads/mrstud/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/mrstud/raw/h264.mp4" },
-  ],
-  /* mrwhitey */
-  [
-    { type: "video/webm", src: "./ads/mrwhitey/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/mrwhitey/raw/h264.mp4" },
-  ],
-  /* naranjita */
-  [
-    { type: "video/webm", src: "./ads/naranjita/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/naranjita/raw/h264.mp4" },
-  ],
-  /* nicola */
-  [
-    { type: "video/webm", src: "./ads/nicola/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/nicola/raw/h264.mp4" },
-  ],
-  /* orgiatic */
-  [
-    { type: "video/webm", src: "./ads/orgiatic/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/orgiatic/raw/h264.mp4" },
-  ],
-  /* peralez */
-  [
-    { type: "video/webm", src: "./ads/peralez/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/peralez/raw/h264.mp4" },
-  ],
-  /* rcs */
-  [
-    { type: "video/webm", src: "./ads/rcs/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/rcs/raw/h264.mp4" },
-  ],
-  /* real_water */
-  [
-    { type: "video/webm", src: "./ads/real_water/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/real_water/raw/h264.mp4" },
-  ],
-  /* slaughterhouse */
-  [
-    { type: "video/webm", src: "./ads/slaughterhouse/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/slaughterhouse/raw/h264.mp4" },
-  ],
-  /* sojasil */
-  [
-    { type: "video/webm", src: "./ads/sojasil/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/sojasil/raw/h264.mp4" },
-  ],
-  /* speed_loans */
-  [
-    { type: "video/webm", src: "./ads/speed_loans/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/speed_loans/raw/h264.mp4" },
-  ],
-  /* techtronika */
-  [
-    { type: "video/webm", src: "./ads/techtronika/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/techtronika/raw/h264.mp4" },
-  ],
-  /* thrud */
-  [
-    { type: "video/webm", src: "./ads/thrud/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/thrud/raw/h264.mp4" },
-  ],
-  /* tiancha */
-  [
-    { type: "video/webm", src: "./ads/tiancha/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/tiancha/raw/h264.mp4" },
-  ],
-  /* uscracks */
-  [
-    { type: "video/webm", src: "./ads/uscracks/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/uscracks/raw/h264.mp4" },
-  ],
-  /* vargas */
-  [
-    { type: "video/webm", src: "./ads/vargas/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/vargas/raw/h264.mp4" },
-  ],
-  /* watson_whore */
-  [
-    { type: "video/webm", src: "./ads/watson_whore/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/watson_whore/raw/h264.mp4" },
-  ],
-  /* wet_dream */
-  [
-    { type: "video/webm", src: "./ads/wet_dream/raw/vp9.webm" },
-    { type: "video/mp4", src: "./ads/wet_dream/raw/h264.mp4" },
-  ],
-];
-
-const aiBumper = [
-  { type: "video/webm", src: "./ads/bumper/ai/vp9.webm" },
-  { type: "video/mp4", src: "./ads/bumper/ai/h264.mp4" },
-];
-
-const rawBumper = [
-  { type: "video/webm", src: "./ads/bumper/raw/vp9.webm" },
-  { type: "video/mp4", src: "./ads/bumper/raw/h264.mp4" },
+const bumper = [
+  { type: "video/webm", src: "./ads/bumper/-AD_TYPE-/vp9.webm" },
+  { type: "video/mp4", src: "./ads/bumper/-AD_TYPE-/h264.mp4" },
 ];
 
 /* doesn't include bumper */
@@ -364,9 +191,8 @@ function setup(player) {
 }
 
 function pickVideo(player) {
-  // TODO: ai vs raw option
   if (adsSinceBumper >= 5) {
-    player.src(aiBumper);
+    player.src(getAdOfPrefType(bumper));
     adsSinceBumper = 0;
     return;
   }
@@ -379,8 +205,7 @@ function pickVideo(player) {
   }
 
   let random = shuffledIndices[currentAdIndex];
-  console.log(random);
-  player.src(aiAds[random]);
+  player.src(getAdOfPrefType(ads[random]));
 
   currentAdIndex++;
 }
@@ -391,4 +216,20 @@ function shuffleArray(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
+}
+
+function getAdOfPrefType(sourceArr) {
+  // TODO: ai vs raw option
+  let ai = true;
+
+  let adType = "raw";
+  if (ai) {
+    adType = "ai";
+  }
+
+  for (let i = 0; i < sourceArr.length; i++) {
+    sourceArr[i].src = sourceArr[i].src.replace("-AD_TYPE-", adType);
+  }
+
+  return sourceArr;
 }
